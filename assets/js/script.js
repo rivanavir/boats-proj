c48f202ab2355d2344fb7e9afd6da0ed86aa9119
Ivan <rivanyshka@gmail.com>
2018-03-24 17:14:21 +0200

    almost finished home page without featured boats carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   
+  $('#recentCarousel').owlCarousel({
+    center: true,
+    items:4,
+    loop: true,
+    margin:26,
+    nav: true,
+    // navText: 'asdf',
+    navText : ["",""],
+    rewindNav : true
+  });
+  
 });
